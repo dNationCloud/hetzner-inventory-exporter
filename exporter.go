bcf146a7db2abb49b010a0dca3d033bf1653f656
Richard Kosegi <richard.kosegi@gmail.com>
2022-03-12 16:39:29 +0100

    Address deprecation of github.com/go-kit/kit
    + go mod tidy

--- exporter.go
+++ exporter.go
@@ -16,8 +16,8 @@ package main
 import (
 	"context"
 
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/hetznercloud/hcloud-go/hcloud"
 	"github.com/prometheus/client_golang/prometheus"
 )
