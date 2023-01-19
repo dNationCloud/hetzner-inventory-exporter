755be98a56a1fa9bad704281c99c1fe502e49a82
Richard Kosegi <richard.kosegi@gmail.com>
2023-01-19 07:28:50 +0100

    Add healthz endpoint

--- main.go
+++ main.go
@@ -123,6 +123,12 @@ func main() {
 `)
 
 	handlerFunc := newHandler(config, logger)
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			panic(err)
+		}
+	})
 	http.Handle(*metricPath, promhttp.InstrumentMetricHandler(prometheus.DefaultRegisterer, handlerFunc))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if _, err = w.Write(landingPage); err != nil {
